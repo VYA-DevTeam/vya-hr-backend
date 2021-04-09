a95c4c968941c0078d43e0f42fc320d3aa143a28
team@willogy.io <team@willogy.io>
2021-04-09 23:53:19 +0700

    first commit

---
+++ db/migrate/20210409161746_create_members.rb
@@ -0,0 +1,13 @@
+class CreateMembers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :members do |t|
+      t.string :name
+      t.string :team
+      t.date :birthday
+      t.integer :gen
+      t.integer :score
+
+      t.timestamps
+    end
+  end
+end
