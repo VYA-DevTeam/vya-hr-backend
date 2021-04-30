00758ae201327f33ded829818e196eab9940e987
Tran My <mychan@Mychans-Macbook-Pro.local>
2021-04-30 22:55:31 +0700

    fix birthday routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :members
   resources :bdayofmonth
+  get '/', to: proc { [200, {}, ['']] }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
