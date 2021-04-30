0155ee8dab7cfbb76cc5b0f3a50b74522872d288
Tran My <mychan@Mychans-Macbook-Pro.local>
2021-04-30 23:31:27 +0700

    edit controller

--- app/controllers/bdayofmonth_controller.rb
+++
@@ -1,7 +0,0 @@
-class BdayofmonthController < ApplicationController
-    
-    def index
-        @members = Member.where("extract(month from birthday) = ?", Date.today.strftime('%m'))
-        render json: @members
-    end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :members
-  get '/birthday' => 'members#birthday' 
-  get '/birthdayofmonth' => 'bdayofmonth#index', to: proc { [200, {}, ['']] }
-
-
+  get '/birthday' => 'members#birthday', to: proc { [200, {}, ['']] }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
