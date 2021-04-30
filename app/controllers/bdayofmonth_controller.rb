1a7912de29368da4c0d304688175e19e688f0646
Tran My <mychan@Mychans-Macbook-Pro.local>
2021-04-30 22:48:08 +0700

    add birthday query

--- app/controllers/bdayofmonth_controller.rb
+++ app/controllers/bdayofmonth_controller.rb
@@ -1,4 +1,5 @@
 class BdayofmonthController < ApplicationController
+    before_action :set_member, only: [:show]
     def index
         @members = Member.where("extract(month from birthday) = ?", Date.today.strftime('%m'))
         render json: @members
