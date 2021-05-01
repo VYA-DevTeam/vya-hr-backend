399e2eaf3def6cc2fe32223665c0da1ad8ef47ee
Tran My <mychan@Mychans-Macbook-Pro.local>
2021-05-01 16:12:38 +0700

    add nickname

--- db/migrate/20210409161746_create_members.rb
+++ db/migrate/20210409161746_create_members.rb
@@ -2,11 +2,11 @@ class CreateMembers < ActiveRecord::Migration[6.1]
   def change
     create_table :members do |t|
       t.string :name
+      t.string :nickname
       t.string :team
       t.date :birthday
       t.integer :gen
       t.integer :score
-
       t.timestamps
     end
   end
